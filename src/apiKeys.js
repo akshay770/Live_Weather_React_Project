9c20f2a888a82bb100d96cb37376a71fdf56bd42
Akshaykumar Aravind Sonar <127863535+akshay770@users.noreply.github.com>
2024-07-15 23:30:19 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,12 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+// module.exports = {
+//   key: '31abb79002dd8469c1203884ce1b182f',
+//   base: "https://api.openweathermap.org/data/2.5/",
+// };
+// apiKeys.js
+const apiKeys = {
+  key: '31abb79002dd8469c1203884ce1b182f',
+  base: 'https://api.openweathermap.org/data/2.5/'
+};
+
+export default apiKeys;
